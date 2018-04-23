205e2788a0d74019a971c94b7eb8592fa66260f5
Yic Zo <yiczo@protonmail.com>
2018-04-23 21:31:53 +0800

    make sure the aircraft stay inside the canvas

--- script/item/aircraft.js
+++ script/item/aircraft.js
@@ -8,19 +8,40 @@ class Aircraft extends Item {
 
     moveUp() {
         this.y -= this.speed
+        this.makeSureInRect()
     }
 
     moveDown() {
         this.y += this.speed
+        this.makeSureInRect()
     }
 
     moveLeft() {
         this.x -= this.speed
+        this.makeSureInRect()
     }
 
     moveRight() {
         this.x += this.speed
+        this.makeSureInRect()
     }
+
+    makeSureInRect() {
+        let canvasWidth = 500
+        let canvasHeight = 500
+
+        if (this.x < 0) {
+            this.x = 0
+        } else if (this.x + this.width > canvasWidth) {
+            this.x = canvasWidth - this.width
+        }
+
+        if (this.y < 0) {
+            this.y = 0
+        } else if (this.y + this.height > canvasHeight) {
+            this.y = canvasHeight - this.height
+        }
+     }
 
     positionShootFrom() {
         let p = {
